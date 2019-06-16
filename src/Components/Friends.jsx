b3a7dd9c72ea3997d2583f9a60434c723b550c72
Shifra Goldstone <sgby1234>
2019-06-16 00:55:02 -0400

    Fixed friends page to work with conditional rendering

--- src/Components/Friends.jsx
+++ src/Components/Friends.jsx
@@ -8,26 +8,42 @@ export class Friends extends Component {
         this.state = { friends: [] }
 
     }
+    
     componentDidMount() {
         console.log("in CMD")
         axios.get("http://localhost:8080/friends").then(res => {
             console.log(res.data)
-            this.setState
-                ({
-                    friends: res.data
-                })
-               
+            this.setState({
+                friends: res.data
+            })
+
         })
     }
+
     render() {
-        const friendElements = this.state.friends.map(friend => <li  className="list-group-item">{friend.userName}</li>)
-        return (
-            <div>
-                <div className = "display-4">Your friends</div>
-                <div className = "lead">Looking for more friends? Click <Link to="/befriend">here</Link></div>
-                <hr classNme="my-4"></hr>
-                {friendElements}
-            </div>
-        )
+        if (this.props.isLoggedIn) {
+            return (<LoggedInPage friends={this.state.friends} />)
+        }
+        else {
+            return (<NotLoggedInPage />)
+        }
     }
+}
+
+function LoggedInPage(props) {
+    const friendElements = props.friends.map(friend => <li className="list-group-item">{friend.userName}</li>)
+    return (
+        <div>
+            <div className="display-4">Your friends</div>
+            <div className="lead">Looking for more friends? Click <Link to="/befriend">here</Link></div>
+            <hr classNme="my-4"></hr>
+            {friendElements}
+        </div>
+    )
+}
+
+function NotLoggedInPage(props) {
+    return (
+        <div> You are not logged in. Click <Link to="/login">here</Link> to log in </div>
+    )
 }
